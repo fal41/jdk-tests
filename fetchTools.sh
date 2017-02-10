534ca96014b33a4d9b5a29a4c417f3c66ab44b85
Klaudiusz Sokolik <klaudiusz.sokolik@igt.com>
2017-02-10 08:36:49 +0100

    path fix

--- fetchTools.sh
+++ fetchTools.sh
@@ -3,7 +3,7 @@ mkdir tools
 cd tools
 wget http://www.gtlib.gatech.edu/pub/apache/tomcat/tomcat-8/v8.0.41/bin/apache-tomcat-8.0.41.zip
 unzip apache-tomcat-8.0.41.zip
-chmod u+x tools/apache-tomcat-8.0.41/bin/*.sh
+chmod u+x apache-tomcat-8.0.41/bin/*.sh
 
 wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/2.2.3/gatling-charts-highcharts-bundle-2.2.3-bundle.zip
 unzip gatling-charts-highcharts-bundle-2.2.3-bundle.zip
